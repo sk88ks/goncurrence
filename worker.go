48715e6ad011c10af072c0c6a1086c228eed0a48
Shun Kokubo <shnkkb@gmail.com>
2015-12-22 19:04:42 +0900

    Changed handling channel

--- worker.go
+++ worker.go
@@ -8,15 +8,16 @@ type ProcessHandler interface {
 
 // WorkerManager is workers manager
 type WorkerManager struct {
-	workerNum   int
-	isUnorderd  bool
-	addCount    int
-	endCount    int
-	errs        []error
-	startSignal chan int
-	endSignal   chan int
-	process     chan ProcessHandler
-	err         chan error
+	workerNum  int
+	isUnorderd bool
+	addCount   int
+	endCount   int
+	errs       []error
+	do         chan struct{}
+	done       chan struct{}
+	end        chan struct{}
+	process    chan ProcessHandler
+	err        chan error
 }
 
 // DefaultProcess is default process implementing process handler
@@ -40,11 +41,12 @@ func (d *DefaultProcess) Exec() error {
 // New creates a new worker manager
 func New(workerNum int) *WorkerManager {
 	w := &WorkerManager{
-		workerNum:   workerNum,
-		startSignal: make(chan int),
-		endSignal:   make(chan int),
-		process:     make(chan ProcessHandler),
-		err:         make(chan error),
+		workerNum: workerNum,
+		do:        make(chan struct{}),
+		done:      make(chan struct{}),
+		end:       make(chan struct{}),
+		process:   make(chan ProcessHandler),
+		err:       make(chan error),
 	}
 
 	// create workers
@@ -56,13 +58,18 @@ func New(workerNum int) *WorkerManager {
 }
 
 func (w *WorkerManager) startWorker() {
-	<-w.startSignal
-	for p := range w.process {
-		err := p.Exec()
-		if err != nil {
-			w.err <- err
+	<-w.do
+	for {
+		select {
+		case p := <-w.process:
+			err := p.Exec()
+			if err != nil {
+				w.err <- err
+			}
+			w.end <- struct{}{}
+		case <-w.done:
+			return
 		}
-		w.endSignal <- 0
 	}
 }
 
@@ -83,10 +90,8 @@ func (w *WorkerManager) Add(p ProcessHandler) *WorkerManager {
 // Run execute all processes
 // If isUnorderd is true and occurred error, last stacked is returned
 func (w *WorkerManager) Run() error {
-	// Send start channel
-	for i := 0; i < w.workerNum; i++ {
-		w.startSignal <- 0
-	}
+	// Start all processes
+	close(w.do)
 
 	for {
 		select {
@@ -95,7 +100,7 @@ func (w *WorkerManager) Run() error {
 			if !w.isUnorderd {
 				return err
 			}
-		case <-w.endSignal:
+		case <-w.end:
 			w.endCount++
 		}
 
